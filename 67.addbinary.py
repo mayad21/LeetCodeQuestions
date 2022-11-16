623dedab88d28fd68803874a19fb658af934d514
Mustapha Ayad <mustapha.ayad8934@gmail.com>
2022-11-16 15:16:06 -0500

    added problem desc

--- 67.addbinary.py
+++ 67.addbinary.py
@@ -1,3 +1,10 @@
+#Given two binary strings a and b, return their sum as a binary string.
+
+#constraints: 
+#1 <= a.length, b.length <= 104
+#a and b consist only of '0' or '1' characters.
+#Each string does not contain leading zeros except for the zero itself.
+
 class Solution(object):
     def addBinary(self, a, b):
         #convert the strings into integers
